ee6a218e4731b47487c66326d4abef92fac376f1
Melkeraug <54627523+Melkeraug@users.noreply.github.com>
2021-03-12 12:40:35 +0100

    js filer

---
+++ Client/Client.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const databaseModule = require('./databaseModule')
+const ThingModel = require('./ThingModel')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '\\staticFiles\\'))
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.post('/test', function (req, res) {
+    ThingModel.saveThing(req.body.kanelbulle)
+
+    res.sendStatus(418)
+})
+
+app.listen(port, () => console.log(`Example app listening on port port!`))
